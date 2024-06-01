75d30a0d39dfbe8f92dc2f2786019b492b3b5ddd
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-01 12:59:55 +0200

    The constant resultText has been removed, and its content has been moved into <p className='result__text'></p>

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,17 +1,13 @@
 import "./style.css";
 
 const Result = ({ amount, currencyGet, currencyHave, conversionedAmount }) => {
-    const resultText = (
-        <>
-            <span>{conversionedAmount} {currencyHave}</span>
-            <span className="result__separator"> = </span>
-            <span>{amount} {currencyGet}</span>
-        </>
-    );
 
     return (
         <div className="result">
-            <p className="result__text">{resultText}</p>
+            <p className="result__text">
+                <span>{conversionedAmount} {currencyHave}</span>
+                <span className="result__separator"> = </span>
+                <span>{amount} {currencyGet}</span></p>
             <p className="result__text result__text--smollerText">
                 Kursy pochodzą ze strony nbp.pl z tabeli nr 102/A/NBP/2024 z dnia 2024-05-27
             </p>
