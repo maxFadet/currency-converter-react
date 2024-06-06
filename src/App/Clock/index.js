72f87fb274279f00cac96967741512a11df2ebd2
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-06 11:59:59 +0200

    replaced function formattedDate outside the Clock component and formatted the code for better readability

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,29 +1,34 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 
+const formattedDate = (time) => time.toLocaleDateString(undefined, {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    day: "numeric",
+    month: "long"
+});
+
 const Clock = () => {
 
     const [time, setTime] = useState(new Date());
 
     useEffect(() => {
-        const timerID = setInterval(() => setTime(new Date()), 1000);
-        return () => clearInterval(timerID);
+        const timerID = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(timerID)
+        };
     }, []);
-
-    const formattedDate = time.toLocaleDateString("pl-PL", {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    });
-
-    const formattedTime = time.toLocaleTimeString();
 
     return (
         <div className="form__date">
             <div>Dzisiaj jest &nbsp;</div>
             <div>
-                {formattedDate},&nbsp;
-                {formattedTime}
+                {formattedDate(time)}
             </div>
         </div>
     );
