b80acfaaa5f014e62d6259c8ac3725928ecd3835
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-11 13:48:19 +0200

    relpaced theme and GlobalStyle in the main index.js file

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,11 @@
 import Container from "./Container";
 import Form from "./Form";
-import { GlobalStyle } from './GlobalStyle';
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
       <Container>
         <Form />
       </Container>
-    </ThemeProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyle } from './App/GlobalStyle';
+import { ThemeProvider } from "styled-components";
+import { theme } from "./App/theme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
