a8aa346b77339e12c197359b11cd827e81950492
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-18 13:40:39 +0200

    The method of fetching data from the server has been changed to 'axios'. Added the data retrieval path "currencies.json" containing cached server data.

--- src/App/Form/useFetchCurrencies.js
+++ src/App/Form/useFetchCurrencies.js
@@ -1,34 +1,41 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 export const useFetchCurrencies = () => {
-    const [currencies, setCurrencies] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [ratesData, setRatesData] = useState({
+        data: null,
+        date: null,
+        loading: false,
+        error: null,
+    });
 
     useEffect(() => {
         const fetchCurrencies = async () => {
-            setLoading(true);
+            setRatesData((previousData) => ({
+                ...previousData,
+                loading: true,
+            }));
             try {
-                const response = await fetch("https://api.exchangerate-api.com/v4/latest/PLN");
-                if (!response.ok) {
-                    throw new Error("Failed to fetch currencies");
-                }
-                const data = await response.json();
-                const formattedCurrencies = Object.keys(data.rates).map(key => ({
-                    short: key,
-                    name: key,
-                    rate: data.rates[key]
-                }));
-                setCurrencies(formattedCurrencies);
+                const response = await axios.get("currencies.json");
+                console.log(response.data);
+
+                setRatesData({
+                    data: response.data.data,
+                    loading: false,
+                    error: null,
+                    date: response.data.meta.last_updated_at,
+                });
             } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }
+                setRatesData((previousData) => ({
+                    ...previousData,
+                    loading: false,
+                    error: error,
+                }));
+            };
         };
 
-        setTimeout(fetchCurrencies, 5000)
+        setTimeout(fetchCurrencies, 2000)
     }, []);
 
-    return { currencies, loading, error };
+    return ratesData;
 };
