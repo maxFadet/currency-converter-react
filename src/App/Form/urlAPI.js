196c44e5a14c7af72b1a10ef90c68d2b8a7e387e
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-18 16:47:44 +0200

    Data update

--- src/App/Form/urlAPI.js
+++ src/App/Form/urlAPI.js
@@ -1,2 +1,2 @@
 export const API =
-    "https://api.currencyapi.com/v3/historical?apikey=cur_live_WL8RsxYK9YiBekejXyNB4kz7fIYa7I8IAa4LM5wN&currencies=EUR%2CUSD%2CCHF%2CPLN";
+    "https://api.currencyapi.com/v3/historical?apikey=cur_live_WL8RsxYK9YiBekejXyNB4kz7fIYa7I8IAa4LM5wN&currencies=EUR%2CUSD%2CCHF%2CPLN&base_currency=PLN&date=2024-06-17";
