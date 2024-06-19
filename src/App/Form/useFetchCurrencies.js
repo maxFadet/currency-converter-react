4644735fb4912c4db8554b943688325339c25c8d
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-19 17:52:04 +0200

    Implemented currency data update every 24 hours

--- src/App/Form/useFetchCurrencies.js
+++ src/App/Form/useFetchCurrencies.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { API } from "./urlAPI";
 import { useState, useEffect } from "react";
 
+const oneDay = 24 * 60 * 60 * 1000;
+
 export const useFetchCurrencies = () => {
     const [ratesData, setRatesData] = useState({
         data: null,
@@ -13,44 +15,55 @@ export const useFetchCurrencies = () => {
     useEffect(() => {
         const fetchCurrencies = async () => {
             const cachedData = localStorage.getItem("currenciesData");
+            const cachedDate = localStorage.getItem("lastFetchDate");
 
-            if (cachedData) {
+            if (cachedData && cachedDate) {
                 const parsedData = JSON.parse(cachedData);
-                setRatesData({
-                    data: parsedData.data,
-                    date: new Date(parsedData.date),
-                    loading: false,
-                    error: null,
-                });
-            } else {
-                try {
-                    const response = await axios.get(API);
+                const lastFetchDate = new Date(cachedDate);
 
-                    const dataToCache = {
-                        data: response.data.data,
-                        date: response.data.meta.last_updated_at
-                    };
-
-                    localStorage.setItem("currenciesData", JSON.stringify(dataToCache));
-
+                if (Date.now() - lastFetchDate.getTime() < oneDay) {
                     setRatesData({
-                        data: response.data.data,
+                        data: parsedData.data,
+                        date: new Date(parsedData.date),
                         loading: false,
                         error: null,
-                        date: new Date(response.data.meta.last_updated_at),
                     });
-                } catch (error) {
-                    setRatesData({
-                        data: null,
-                        date: null,
-                        loading: false,
-                        error: error.message,
-                    });
+                    return;
+                }
+            }
+
+            try {
+                const response = await axios.get(API);
+
+                const dataToCache = {
+                    data: response.data.data,
+                    date: response.data.meta.last_updated_at
                 };
-            };
+
+                localStorage.setItem("currenciesData", JSON.stringify(dataToCache));
+                localStorage.setItem("lastFetchDate", new Date().toISOString());
+
+                setRatesData({
+                    data: response.data.data,
+                    loading: false,
+                    error: null,
+                    date: new Date(response.data.meta.last_updated_at),
+                });
+            } catch (error) {
+                setRatesData({
+                    data: null,
+                    date: null,
+                    loading: false,
+                    error: error.message,
+                });
+            }
         };
 
-        setTimeout(fetchCurrencies, 2000)
+        fetchCurrencies();
+
+        const intervalId = setInterval(fetchCurrencies, oneDay);
+
+        return () => clearInterval(intervalId);
     }, []);
 
     return ratesData;
